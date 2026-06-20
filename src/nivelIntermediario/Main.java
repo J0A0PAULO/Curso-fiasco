6cf57d462bd05feeace0384bb6b15dc17ceeac7c
João <moreirajoaopaulo951@gmail.com>
2026-06-20 00:07:57 -0300

    20/06/2026

--- src/nivelIntermediario/Main.java
+++ src/nivelIntermediario/Main.java
@@ -3,14 +3,7 @@ package nivelIntermediario;
 public class Main {
     static void main(String[] args) {
 
-        System.out.println("-------- Naruto Uzumaki ----------");
-        Uzumaki naruto = new Uzumaki("Naruto Uzumaki", "Aldeia da Folha", 17, 30, 1.67);
-        System.out.println("Meu nome é " + naruto.getNome());
-        naruto.setNome("Naruto Uzumaki com nome alterado");
-
-
-        System.out.println("---------- Sasuke Uchiha ----------");
-
-
+        Missoes missao01 = new Missoes("Resgatar Cachorro", RankDeMissoes.S);
+        missao01.exibirDetalhes();
     }
 }
